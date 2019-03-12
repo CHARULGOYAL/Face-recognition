acaca1b8f08ea67252c6c58cd813b0a9afc9a297
CHARUL GOYAL <cgcharulgoyal.52@gmail.com>
2019-03-12 22:13:25 +0530

    First commit

---
+++ findfaces_in_pictures.py
@@ -0,0 +1,45 @@
+#import face_recognition
+#image = face_recognition.load_image_file("charul.jpg")
+#face_locations = face_recognition.face_locations(image)
+#face_landmarks_list = face_recognition.face_landmarks(image)
+from PIL import Image, ImageDraw
+import face_recognition
+import numpy as np
+
+# Load the jpg file into a numpy array
+image = face_recognition.load_image_file("unknown.jpg")
+
+# Find all the faces in the image using the default HOG-based model.
+# This method is fairly accurate, but not as accurate as the CNN model and not GPU accelerated.
+# See also: find_faces_in_picture_cnn.py
+face_locations = face_recognition.face_locations(image,  model="cnn")
+
+print("I found {} face(s) in this photograph.".format(len(face_locations)))
+
+for face_location in face_locations:
+     
+    # Print the location of each face in this image
+        top, right, bottom, left = face_location
+        print("A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}".format(top, left, bottom, right))
+ 
+    # You can access the actual face itself like this:
+        face_image = image[top:bottom, left:right]
+       
+        pil_image = Image.fromarray(face_image)
+        d = ImageDraw.Draw(pil_image)
+
+        face_landmarks_list = face_recognition.face_landmarks(face_image)
+
+        for face_landmarks in face_landmarks_list:
+   
+    # Print the location of each facial feature in this image
+            for facial_feature in face_landmarks.keys():
+                print("The {} in this face has the following points: {}".format(facial_feature, face_landmarks[facial_feature]))
+
+    # Let's trace out each facial feature in the image with a line!
+            for facial_feature in face_landmarks.keys():
+                d.line(face_landmarks[facial_feature], width=5)
+
+        pil_image.show()
+
+
